22488f596f62203dbdc454f656d49eacba14df52
Dula21 <st20259983@outlook.cardiffmet.ac.uk>
2024-12-02 15:21:07 +0400

    add some features

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
-const { MongoClient, ObjectId } = require('mongodb'); // Use the new MongoClient import
+const { MongoClient, ObjectId } = require('mongodb'); // Correct import
 const path = require("path");
-
-
 
 const app = express();
 
@@ -10,35 +8,22 @@ app.use(express.json());
 app.set('port', 3000);
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
-    );
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD, OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
 });
 
 // MongoDB connection
-const uri = 'mongodb+srv://Admin:admin@gettingstarted.quhps.mongodb.net/';
-const client = new MongoClient(uri);
-
 let db;
-
-// Connect to MongoDB
-async function connectToMongoDB() {
-    try {
-        await client.connect(); // Connect to the MongoDB server
-        db = client.db('Lessons'); // Use the database 'webstore'
-        console.log('Connected to MongoDB successfully');
-    } catch (error) {
-        console.error('Failed to connect to MongoDB', error);
-        process.exit(1); // Exit if the connection fails
+MongoClient.connect('mongodb+srv://Admin:admin@gettingstarted.quhps.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+    if (err) {
+        console.error('Failed to connect to the database:', err);
+        return;
     }
-}
-
-// Start MongoDB connection
-connectToMongoDB();
+    db = client.db('Lessons');
+    console.log('Connected to Database');
+});
 
 // Routes
 app.get('/', (req, res) => {
@@ -50,62 +35,71 @@ app.param('collectionName', (req, res, next, collectionName) => {
     return next();
 });
 
-app.get('/collection/:collectionName', async (req, res, next) => {
-    try {
-        const results = await req.collection.find({}).toArray();
+app.get('/collection/:collectionName', (req, res, next) => {
+    req.collection.find({}).toArray((e, results) => {
+        if (e) return next(e);
         res.send(results);
-    } catch (error) {
-        next(error);
-    }
+    });
 });
 
-app.post('/collection/:collectionName', async (req, res, next) => {
-    try {
-        const result = await req.collection.insertOne(req.body);
-        res.send(result.ops);
-    } catch (error) {
-        next(error);
-    }
+// Adding POST
+app.post('/collection/:collectionName', (req, res, next) => {
+    req.collection.insert(req.body, (e, results) => {
+        if (e) return next(e);
+        res.send(results.ops);
+    });
 });
 
-app.put('/collection/:collectionName/:id', async (req, res, next) => {
-    try {
-        const result = await req.collection.updateOne(
-            { _id: new ObjectId(req.params.id) },
-            { $set: req.body }
-        );
-        res.send(result.modifiedCount === 1 ? { msg: 'success' } : { msg: 'error' });
-    } catch (error) {
-        next(error);
+// PUT method to update an object
+app.put('/collection/:collectionName/:id', (req, res, next) => {
+    const id = req.params.id;
+    if (!ObjectId.isValid(id)) {
+        return res.status(400).json({ message: 'Invalid ID format' });
     }
+    req.collection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: req.body },
+        (e, result) => {
+            if (e) return next(e);
+            res.json(result.result.n === 1 ? { msg: 'success' } : { msg: 'error' });
+        }
+    );
 });
 
-app.delete('/collection/:collectionName/:id', async (req, res, next) => {
-    try {
-        const result = await req.collection.deleteOne({ _id: new ObjectId(req.params.id) });
-        res.send(result.deletedCount === 1 ? { msg: 'success' } : { msg: 'error' });
-    } catch (error) {
-        next(error);
+// DELETE method to remove an object
+app.delete('/collection/:collectionName/:id', (req, res, next) => {
+    const id = req.params.id;
+    if (!ObjectId.isValid(id)) {
+        return res.status(400).json({ message: 'Invalid ID format' });
     }
+    req.collection.deleteOne(
+        { _id: new ObjectId(id) },
+        (e, result) => {
+            if (e) return next(e);
+            res.json(result.result.n === 1 ? { msg: 'success' } : { msg: 'error' });
+        }
+    );
 });
 
-app.use('/static', express.static(path.join(__dirname, 'static'))); // Serve static files
+// Serve static files
+app.use('/static', express.static(path.join(__dirname, 'static')));
 
 // Serve index.html
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Catch-all for other routes (for SPAs or client-side routing)
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
+    console.error(err.stack);
+    res.status(500).json({ message: 'Something broke!' });
 });
+
 // Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
